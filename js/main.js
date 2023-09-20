1ac34eeabe8c9e1063d5b8d739d9ee9ceff21b14
Maxim Datskiy <datskiumax@gmail.com>
2023-09-20 16:21:21 -0500

    Complete desktop version for section "feedback"

--- js/main.js
+++ js/main.js
@@ -4,3 +4,10 @@ const menuMobile = document.querySelector('.header__menu-list')
 menuBtn.addEventListener('click', ()=> {
     menuMobile.classList.toggle('menu--open');
 })
+
+const swiperFeedback = new Swiper('.feedback__slider', {
+    loop: true,
+    pagination: {
+        el: '.swiper-pagination',
+    },
+});
